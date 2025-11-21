434987322de5bab98231508b0d7c46027ccaf9ca
digitaldagstudio <digital.dag.studio@gmail.com>
2025-11-20 19:58:55 -0600

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,12 @@
+const posts = [
+  {
+    id: 1,
+    title: "5-Step Morning Routine for Low-Energy Days",
+    date: "2025-02-20",
+    content: `
+      <p>This is where your full blog post goes...</p>
+      <p>You can use HTML, images, lists, etc.</p>
+    `,
+    cover: "images/post1.jpg"
+  }
+];
