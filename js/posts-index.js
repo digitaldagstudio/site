200a255e2c656e9103af4ed32648b02c32c90243
digitaldagstudio <digital.dag.studio@gmail.com>
2025-11-24 18:01:14 -0600

    Fix syntax error in postScriptFiles array

--- js/posts-index.js
+++ js/posts-index.js
@@ -1,7 +1,8 @@
 // This is the only place that knows which post files exist
 window.postScriptFiles = [
   "posts/adhd-morning-routine.js",
-  "posts/dog-hiking-essentials.js"
+  "posts/dog-hiking-essentials.js",
   "posts/adhd-weekend-reset.js"
   // add new post files here
+
 ];
