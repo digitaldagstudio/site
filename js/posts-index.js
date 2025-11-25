3085348d05bd42a9102b38145a875177782d4620
digitaldagstudio <digital.dag.studio@gmail.com>
2025-11-24 21:58:21 -0600

    Update posts-index.js

--- js/posts-index.js
+++ js/posts-index.js
@@ -3,7 +3,8 @@ window.postScriptFiles = [
   "posts/adhd-morning-routine.js",
   "posts/dog-hiking-essentials.js",
   "posts/adhd-weekend-reset.js",
-  "posts/thanksgiving-week-adhd-to-do-list-no-burnout-edition.js"
+  "posts/thanksgiving-week-adhd-to-do-list-no-burnout-edition.js",
+  "thanksgiving-week-walk-routine-anxious-pups.js"
   // add new post files here
 
 ];
